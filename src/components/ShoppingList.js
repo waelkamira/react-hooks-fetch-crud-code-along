f51c1edee60ee1087b1058b5d5e73b1c4919ce61
wael kamira <ramondm4@gmail.com>
2023-06-26 13:10:43 +0300

    Assignment Completed

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,34 +1,69 @@
-import React, { useState } from "react";
-import ItemForm from "./ItemForm";
-import Filter from "./Filter";
-import Item from "./Item";
+import React, { useEffect, useState } from "react";
 
 function ShoppingList() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
 
-  function handleCategoryChange(category) {
-    setSelectedCategory(category);
-  }
+  useEffect(() => {
+    fetch("https://api.example.com/shopping-items")
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+      .catch((error) => {
+        console.error("Error fetching shopping items:", error);
+      });
+  }, []);
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const handleAddItem = (name, category) => {
+    const newItem = {
+      id: Date.now(),
+      name,
+      category,
+      isInCart: false,
+    };
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
 
-    return item.category === selectedCategory;
-  });
+  const handleToggleCart = (itemId) => {
+    setItems((prevItems) =>
+      prevItems.map((item) =>
+        item.id === itemId ? { ...item, isInCart: !item.isInCart } : item
+      )
+    );
+  };
+
+  const handleDeleteItem = (itemId) => {
+    setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+  };
 
   return (
-    <div className="ShoppingList">
-      <ItemForm />
-      <Filter
-        category={selectedCategory}
-        onCategoryChange={handleCategoryChange}
-      />
-      <ul className="Items">
-        {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+    <div>
+      <h1>Shopping List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name} - {item.category}
+            <button onClick={() => handleToggleCart(item.id)}>
+              {item.isInCart ? "Remove From Cart" : "Add to Cart"}
+            </button>
+            <button onClick={() => handleDeleteItem(item.id)}>Delete</button>
+          </li>
         ))}
       </ul>
+      <h2>Add Item</h2>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          const name = e.target.elements.name.value;
+          const category = e.target.elements.category.value;
+          handleAddItem(name, category);
+          e.target.reset();
+        }}
+      >
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" />
+        <label htmlFor="category">Category:</label>
+        <input type="text" id="category" />
+        <button type="submit">Add to List</button>
+      </form>
     </div>
   );
 }
